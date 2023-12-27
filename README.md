9dcb92aae9a202dc8398c0b119a6a6d791a9e1f3
Jai Luthra <me@jailuthra.in>
2023-12-27 16:58:12 +0530

    Fix title, add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chord Builder
+
+Inspired by [Guitarmadillo's Chord Generator](https://guitarmadillo.com/guitar-chords).
+
+Rust-based [chord-generator project](https://github.com/jailuthra/chord-generator) provides the input JSON data with all the chord inversions.
--- build.py
+++ build.py
@@ -3,6 +3,6 @@ from staticjinja import Site
 if __name__ == "__main__":
     notes = ['C','CSharp','D','DSharp','E','F','FSharp','G','GSharp','A','ASharp','B']
     variations = ['Major','Minor','Augmented','Diminished','Seventh','MajorSeventh','MinorSeventh','Sus2','Sus4','MinorMajorSeventh','DiminishedSeventh','MajorNinth','MinorNinth','AddNinth','AddEleventh','MinorSixth','MajorSixth','AddSixthAddNinth']
-    context = {'notes': notes, 'variations' : variations}
+    context = {'notes': notes, 'variations' : variations, 'title': 'Chord Builder'}
     site = Site.make_site(contexts=[('index.html', context)])
     site.render()
